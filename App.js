5ea312c90824049af9c00977afd13180a6383c94
RekoLep <reko.leppanen@myy.haaga-helia.fi>
2025-10-27 19:25:24 +0200

    lisäyksiä taas

--- App.js
+++ App.js
@@ -1,16 +1,42 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import HomeScreen from './src/HomeScreen'
-import SettingScreen from './src/SettingScreen'
-  
+import { Text } from 'react-native';
+import HomeScreen from './src/HomeScreen';
+import SettingScreen from './src/SettingScreen';
+
 const Tab = createBottomTabNavigator();
-  
+
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Koti" component={HomeScreen} />
-        <Tab.Screen name="Asetukset" component={SettingScreen} />
+      <Tab.Navigator
+        screenOptions={{
+          headerShown: false, 
+          tabBarLabelStyle: { fontSize: 12 },
+        }}
+      >
+        <Tab.Screen 
+          name="Koti" 
+          component={HomeScreen}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <Text style={{ fontSize: 22 }}>
+                {'🏠'}
+              </Text>
+            ),
+          }}
+        />
+        <Tab.Screen 
+          name="Asetukset" 
+          component={SettingScreen}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <Text style={{ fontSize: 22 }}>
+                {'⚙️'}
+              </Text>
+            ),
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
