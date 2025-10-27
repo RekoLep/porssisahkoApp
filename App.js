5b3da9fb6a38f65b3b338e222bbd5906c893c157
RekoLep <reko.leppanen@myy.haaga-helia.fi>
2025-10-27 11:49:43 +0200

    lisäyksiä

--- App.js
+++ App.js
@@ -1,63 +1,17 @@
-
-import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { fetchLatestPriceData, getPriceForDate } from './src/api/currentPriceApi';
-
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from './src/HomeScreen'
+import SettingScreen from './src/SettingScreen'
+  
+const Tab = createBottomTabNavigator();
+  
 export default function App() {
-  const [price, setPrice] = useState(null);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    async function loadPrice() {
-      try {
-        const { prices } = await fetchLatestPriceData();
-        const now = new Date();
-        const p = getPriceForDate(now, prices);
-        setPrice(p);
-      } catch (e) {
-        setError(e.message);
-      }
-    }
-
-    loadPrice();
-  }, []);
-
-  if (error) {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.error}>Virhe: {error}</Text>
-      </View>
-    );
-  }
-
-  if (price === null) {
-    return (
-      <View style={styles.container}>
-        <Text>Ladataan hintaa...</Text>
-      </View>
-    );
-  }
-
   return (
-    <View style={styles.container}>
-      <Text style={styles.priceText}>
-        Hinta nyt: {price} snt / kWh (sis. alv)
-      </Text>
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Koti" component={HomeScreen} />
+        <Tab.Screen name="Asetukset" component={SettingScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  priceText: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  error: {
-    color: 'red',
-  },
-});
---
+++ src/HomeScreen.js
@@ -0,0 +1,63 @@
+
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { fetchLatestPriceData, getPriceForDate } from './api/CurrentPriceApi'
+
+export default function HomeScreen() {
+  const [price, setPrice] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function loadPrice() {
+      try {
+        const { prices } = await fetchLatestPriceData();
+        const now = new Date();
+        const p = getPriceForDate(now, prices);
+        setPrice(p);
+      } catch (e) {
+        setError(e.message);
+      }
+    }
+
+    loadPrice();
+  }, []);
+
+  if (error) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.error}>Virhe: {error}</Text>
+      </View>
+    );
+  }
+
+  if (price === null) {
+    return (
+      <View style={styles.container}>
+        <Text>Ladataan hintaa...</Text>
+      </View>
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.priceText}>
+        Hinta nyt: {price} snt / kWh (sis. alv)
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  priceText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  error: {
+    color: 'red',
+  },
+});
